02ee24e7fb34aefd6c1edb45bad3fb86f26d305f
Travis912 <110688507+Travis912@users.noreply.github.com>
2025-03-22 14:18:12 -0400

    vite react

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    jsxInject: `import React from 'react'`,
-  },
+  base: "/OfficialPortfolio2",
 });
