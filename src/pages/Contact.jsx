9c057a1db01a472e2b33cc1cc85e640bd8c19a7f
Travis912 <110688507+Travis912@users.noreply.github.com>
2025-03-26 14:47:16 -0400

    Form - CSS - and svg test

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,28 @@
 import '../styles/Contact.css';
+import emailjs from '@emailjs/browser';
+import { useRef } from 'react';
 
 const Contact = () => {
+
+    const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_lyc1mrm', 'template_ugwfonr', form.current, {
+        publicKey: 'Z-1JEP5h3M5CBotBx',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
     return (
         <section className='enitre-contact-section'>
             <div className="contact-page">
@@ -13,28 +35,28 @@ const Contact = () => {
                         </div>
                         <p>Like what you see? Have feedback on how I can improve? Contact me for any hiring inquires or to leave feedback. </p>
                     </div>
-                    <form className='contact-form-container'>
+                    <form className='contact-form-container' ref={form} onSubmit={sendEmail}>
                         <div className='contact-form-line'>
                             <label className="" htmlFor="name">Hi, I&apos;m </label>
-                            <input className="contact-form-input" type="text" id="name" name="name" placeholder='Name*' required />
+                            <input className="contact-form-input" type="text" id="name" name="from_name" placeholder='Name*' required />
                         </div>
                         <div className='contact-form-line'>
                             <label htmlFor="company">and I work with </label>
-                            <input className="contact-form-input" type="email" id="company" name="company" placeholder='Company*' required />
+                            <input className="contact-form-input" type="text" id="company" name="user_company" placeholder='Company*' required />
                         </div>
                         <div className='contact-form-line'>
                             <label htmlFor="Phone">You can reach me at </label>
-                            <input className="contact-form-input" type="Phone" id="Phone" name="Phone" placeholder='Phone*' />
+                            <input className="contact-form-input" type="Phone" id="Phone" name="user_phone" placeholder='Phone*' />
                         </div>
                         <div className='contact-form-line'>
                             <label htmlFor="email">or at </label>
-                            <input className="contact-form-input" type="email" id="email" name="email" placeholder='Email*' required />
+                            <input className="contact-form-input" type="email" id="email" name="user_email" placeholder='Email*' required />
                         </div>
                         <div className='contact-form-line'>
                             <label htmlFor="message"></label>
                             <textarea className="contact-form-input" id="message" name="message" placeholder='Message*' required></textarea>
                         </div>
-                        <button className="contact-submit-btn" type="submit">Send Message</button>
+                        <button className="contact-submit-btn" type="submit" value="Send">Send Message</button>
                     </form>
                 </div>
             </div>
--- src/projects/banff/banffNav.jsx
+++ src/projects/banff/banffNav.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import banffLogo from './banffLogo-new.png';
 import BanffNavDropdown from './banffNavDropdown';
 
@@ -63,18 +62,38 @@ export default function BanffNav() {
             <nav className="banff-navbar">
                 <div className='banff-above-nav-div'>
                     <ul>
-                        <li> <i className="fa-solid fa-circle-user"> </i> Login </li>
-                        <li> <i className="fa-solid fa-store"> </i> Buy Your Park Pass</li>
-                        <li> <i className="fa-solid fa-cloud"> </i> Weather & Webcams </li>
-                        <li> <i className="fa-solid fa-calendar"> </i> What&apos;s Happening </li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                                <path fillRule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clipRule="evenodd" />
+                            </svg>
+                            Login
+                        </li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 21v-7.5a.75.75 0 0 1 .75-.75h3a.75.75 0 0 1 .75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349M3.75 21V9.349m0 0a3.001 3.001 0 0 0 3.75-.615A2.993 2.993 0 0 0 9.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 0 0 2.25 1.016c.896 0 1.7-.393 2.25-1.015a3.001 3.001 0 0 0 3.75.614m-16.5 0a3.004 3.004 0 0 1-.621-4.72l1.189-1.19A1.5 1.5 0 0 1 5.378 3h13.243a1.5 1.5 0 0 1 1.06.44l1.19 1.189a3 3 0 0 1-.621 4.72M6.75 18h3.75a.75.75 0 0 0 .75-.75V13.5a.75.75 0 0 0-.75-.75H6.75a.75.75 0 0 0-.75.75v3.75c0 .414.336.75.75.75Z" />
+                            </svg>
+                            Buy Your Park Pass</li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
+                            </svg>
+                            Weather & Webcams
+                        </li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 2.994v2.25m10.5-2.25v2.25m-14.252 13.5V7.491a2.25 2.25 0 0 1 2.25-2.25h13.5a2.25 2.25 0 0 1 2.25 2.25v11.251m-18 0a2.25 2.25 0 0 0 2.25 2.25h13.5a2.25 2.25 0 0 0 2.25-2.25m-18 0v-7.5a2.25 2.25 0 0 1 2.25-2.25h13.5a2.25 2.25 0 0 1 2.25 2.25v7.5m-6.75-6h2.25m-9 2.25h4.5m.002-2.25h.005v.006H12v-.006Zm-.001 4.5h.006v.006h-.006v-.005Zm-2.25.001h.005v.006H9.75v-.006Zm-2.25 0h.005v.005h-.006v-.005Zm6.75-2.247h.005v.005h-.005v-.005Zm0 2.247h.006v.006h-.006v-.006Zm2.25-2.248h.006V15H16.5v-.005Z" />
+                            </svg>
+                            What&apos;s Happening
+                        </li>
                     </ul>
                 </div>
 
                 <div className='banff-lower-nav-div' aria-expanded={isMenuOpen}>
 
                     <div className="banff-hamburger-icon" onClick={toggleMenu}>
-                        <FontAwesomeIcon icon={isMenuOpen ? "fa-solid fa-x" : "fas fa-bars"} />
-                        <i className={isMenuOpen ? "fa-solid fa-x" : "fas fa-bars"}></i>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                        </svg>
                     </div>
                     <BanffNavDropdown className="BanffNavDropdown-mobile" mobileMenuOpen={isMenuOpen}/>
 
